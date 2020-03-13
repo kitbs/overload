2e844d714140f6e873831cdef2f4642d4c33fa1c
Kit Burton-Senior <mail@kitbs.com>
2020-03-13 14:54:59 +0000

    Revert "fallback for no matching overloads"
    This reverts commit e9be276c1b5075b1f5fa5646d580ae25b4437233.

--- src/Overloadable.php
+++ src/Overloadable.php
@@ -2,52 +2,16 @@
 
 namespace Tightenco\Overload;
 
-use Closure;
-use InvalidArgumentException;
 use Tightenco\Collect\Support\Collection;
 
 trait Overloadable
 {
-    public function overload($args, $signatures, $fallback = null)
+    public function overload($args, $signatures)
     {
-        $candidate = (new Collection($signatures))->map(function ($value, $key) {
+        return (new Collection($signatures))->map(function ($value, $key) {
             return new OverloadedMethodCandidate($value, $key, $this);
         })->first(function ($candidate) use ($args) {
             return $candidate->matches($args);
-        });
-
-        if (!$candidate instanceof OverloadedMethodCandidate) {
-            if (func_num_args() == 3) {
-                if ($fallback instanceof Closure) {
-                    $fallback->bindTo($this);
-                    return $fallback($args);
-                }
-                return $fallback;
-            }
-
-            $types = (new Collection($args))->map(function ($arg) {
-                $type = gettype($arg);
-
-                if ($type == 'object') {
-                    $type = get_class($arg);
-                }
-
-                return $type;
-            });
-
-            $count = $types->count();
-            $s = $count == 1 ? '' : 's';
-
-            $message = '';
-
-            if ($count) {
-                $types = $types->implode(', ');
-                $message = " of type$s $types";
-            }
-
-            throw new InvalidArgumentException("No overloaded method found for $count argument$s$message");
-        }
-
-        return $candidate->call($args);
+        })->call($args);
     }
 }
